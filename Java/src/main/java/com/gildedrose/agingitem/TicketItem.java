c1458ca5ff3f4517b58876a91e56fb10b1450870
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 16:22:37 +0100

    Refactor TicketItem to use MAX_ITEM_QUALITY

--- Java/src/main/java/com/gildedrose/agingitem/TicketItem.java
+++ Java/src/main/java/com/gildedrose/agingitem/TicketItem.java
@@ -1,31 +1,34 @@
 package com.gildedrose.agingitem;
 
 public class TicketItem extends AgingItem {
+    private static final int MAX_ITEM_QUALITY = 50;
+
     public TicketItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     @Override
     public void increaseAge() {
-        if (this.quality < 50) {
-            this.quality = this.quality + 1;
-            if (this.sellIn < 11) {
-                if (this.quality < 50) {
-                    this.quality = this.quality + 1;
-                }
-            }
-
-            if (this.sellIn < 6) {
-                if (this.quality < 50) {
-                    this.quality = this.quality + 1;
-                }
-            }
-        }
-
-        this.sellIn = this.sellIn - 1;
-
+        this.sellIn--;
         if (this.sellIn < 0) {
             this.quality = 0;
+            return;
+        }
+        if (this.quality >= MAX_ITEM_QUALITY) {
+            return;
+        }
+        this.quality = this.quality + 1;
+        if (this.quality >= MAX_ITEM_QUALITY) {
+            return;
+        }
+        if (this.sellIn < 10) {
+            this.quality = this.quality + 1;
+            if (this.quality >= MAX_ITEM_QUALITY) {
+                return;
+            }
+        }
+        if (this.sellIn < 5) {
+            this.quality = this.quality + 1;
         }
     }
 }
