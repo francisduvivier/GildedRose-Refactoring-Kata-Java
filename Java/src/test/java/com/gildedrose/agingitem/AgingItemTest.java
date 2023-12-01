21d3de6b7b263aab233488b64b425e2ead84327d
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 09:13:15 +0100

    Add test to check that item quality decreases at double speed when sell in date has passed

--- Java/src/test/java/com/gildedrose/agingitem/AgingItemTest.java
+++ Java/src/test/java/com/gildedrose/agingitem/AgingItemTest.java
@@ -6,12 +6,18 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class AgingItemTest {
 
+    /**
+     * All items have a Quality value which denotes how valuable the item is
+     */
     @Test
     public void qualityIsAccessible() {
         AgingItem item = new AgingItem("general", 10, 20);
         assertEquals(20, item.getQuality());
     }
 
+    /**
+     * At the end of each day our system lowers both values for every item
+     */
     @Test
     public void decreasesQualityByOneWhenAgesBeforeExpiry() {
         int startQuality = 20;
@@ -22,6 +28,10 @@ public class AgingItemTest {
         assertEquals(startQuality - 1, item.getQuality());
     }
 
+    /**
+     * Edge case test for:
+     * At the end of each day our system lowers both values for every item
+     */
     @Test
     public void decreasesQualityByOneWhenAgesRightBeforeExpiry() {
         int startQuality = 20;
@@ -33,6 +43,9 @@ public class AgingItemTest {
         assertEquals(startQuality - startSellIn, item.getQuality());
     }
 
+    /**
+     * The Quality of an item is never negative
+     */
     @Test
     public void neverDecreasesQualityBelowZero() {
         int startQuality = 20;
@@ -40,6 +53,39 @@ public class AgingItemTest {
         AgingItem item = new AgingItem("general", startSellIn, startQuality);
 
         item.increaseAge(startQuality + 10);
+
+        assertEquals(0, item.getQuality());
+    }
+
+    /**
+     * Once the sell by date has passed, Quality degrades twice as fast
+     */
+    @Test
+    public void decreaseQualityAtDoubleSpeedWhenSellInHasPassed() {
+        int startQuality = 20;
+        int startSellIn = 10;
+        AgingItem item = new AgingItem("general", startSellIn, startQuality);
+
+        int extraAgeAfterSellInPassed = 3;
+        item.increaseAge(startSellIn + extraAgeAfterSellInPassed);
+
+        int expectedQuality = startQuality - (startSellIn + 2 * extraAgeAfterSellInPassed);
+        assertEquals(expectedQuality, item.getQuality());
+    }
+
+
+    /**
+     * Edge case test for:
+     * Once the sell by date has passed, Quality degrades twice as fast
+     */
+    @Test
+    public void decreaseQualityAtDoubleSpeedToZeroWhenSellInHasPassed() {
+        int startQuality = 19;
+        int startSellIn = 10;
+        AgingItem item = new AgingItem("general", startSellIn, startQuality);
+
+        int minimalDaysToZeroAfterSellInPassed = (int) Math.ceil((startQuality - startSellIn) / 2.0);
+        item.increaseAge(startSellIn + minimalDaysToZeroAfterSellInPassed);
 
         assertEquals(0, item.getQuality());
     }
