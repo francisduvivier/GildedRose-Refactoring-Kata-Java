25e6c78cc7348475f0ba58913c394d7409e79916
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 16:53:17 +0100

    Refactor StandardAgingItem.java to allow passing qualityUpdateMultiplier from subclass

--- Java/src/main/java/com/gildedrose/agingitem/ConjuredItem.java
+++ Java/src/main/java/com/gildedrose/agingitem/ConjuredItem.java
@@ -2,13 +2,6 @@ package com.gildedrose.agingitem;
 
 public class ConjuredItem extends StandardAgingItem {
     public ConjuredItem(String name, int sellIn, int quality) {
-        super(name, sellIn, quality);
-    }
-
-    @Override
-    public void increaseAge() {
-        super.increaseAge();
-        this.sellIn++;
-        super.increaseAge();
+        super(name, sellIn, quality, 2);
     }
 }
