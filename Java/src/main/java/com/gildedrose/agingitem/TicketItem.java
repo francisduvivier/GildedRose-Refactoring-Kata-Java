6c3a9a9368bf29c065658efd7111f8d9eafeb5c9
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 15:57:19 +0100

    Create TicketItem test cases

---
+++ Java/src/main/java/com/gildedrose/agingitem/TicketItem.java
@@ -0,0 +1,12 @@
+package com.gildedrose.agingitem;
+
+public class TicketItem extends AgingItem {
+    public TicketItem(String name, int sellIn, int quality) {
+        super(name, sellIn, quality);
+    }
+
+    @Override
+    public void increaseAge() {
+        // TODO
+    }
+}
--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -12,9 +12,9 @@ public class TexttestFixture {
             new StandardAgingItem("Elixir of the Mongoose", 5, 7), //
             new LegendaryItem("Sulfuras, Hand of Ragnaros", 0, 80), //
             new LegendaryItem("Sulfuras, Hand of Ragnaros", -1, 80),
-            new CustomAgingItem("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-            new CustomAgingItem("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-            new CustomAgingItem("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new TicketItem("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new TicketItem("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new TicketItem("Backstage passes to a TAFKAL80ETC concert", 5, 49),
             // this conjured item does not work properly yet
             new StandardAgingItem("Conjured Mana Cake", 3, 6)};
 
