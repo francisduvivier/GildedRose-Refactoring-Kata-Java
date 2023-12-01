96915fbb2c36cc6565b602638e2d057362ca74eb
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 16:08:47 +0100

    Move code for Backstage passes to a TAFKAL80ETC concert to TicketItem class to implement TickeItem

--- Java/src/main/java/com/gildedrose/agingitem/TicketItem.java
+++ Java/src/main/java/com/gildedrose/agingitem/TicketItem.java
@@ -7,6 +7,25 @@ public class TicketItem extends AgingItem {
 
     @Override
     public void increaseAge() {
-        // TODO
+        if (this.quality < 50) {
+            this.quality = this.quality + 1;
+            if (this.sellIn < 11) {
+                if (this.quality < 50) {
+                    this.quality = this.quality + 1;
+                }
+            }
+
+            if (this.sellIn < 6) {
+                if (this.quality < 50) {
+                    this.quality = this.quality + 1;
+                }
+            }
+        }
+
+        this.sellIn = this.sellIn - 1;
+
+        if (this.sellIn < 0) {
+            this.quality = 0;
+        }
     }
 }
