e740e2e08c0c46b7acb636f85be819ca45069224
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 10:56:08 +0100

    Add test to check that GildedRose class calls increaseAge correctly

--- Java/src/test/java/com/gildedrose/GildedRoseTest.java
+++ Java/src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,19 +1,50 @@
 package com.gildedrose;
 
 import com.gildedrose.agingitem.AgingItem;
-import com.gildedrose.agingitem.StandardAgingItem;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class GildedRoseTest {
 
+    private static class SpiedUponAgingItem extends AgingItem {
+        int totalAgeIncrease = 0;
+
+        public SpiedUponAgingItem(String name, int sellIn, int quality) {
+            super(name, sellIn, quality);
+        }
+
+        @Override
+        public void increaseAge(int amount) {
+            totalAgeIncrease += amount;
+        }
+    }
+
     @Test
-    void foo() {
-        AgingItem[] items = new AgingItem[] { new StandardAgingItem("foo", 0, 0) };
+    void keepsItems() {
+        AgingItem[] items = new AgingItem[]{
+            new SpiedUponAgingItem("foo", 0, 0),
+            new SpiedUponAgingItem("bar", 0, 0),
+        };
         GildedRose app = new GildedRose(items);
+
         app.updateQuality();
-        assertEquals("foo", app.items[0].name);
+
+        assertEquals(items, app.items);
+    }
+
+    @Test
+    void callsIncreaseAgeOnEveryItemWhenCallingUpdateQuality() {
+        SpiedUponAgingItem spy = new SpiedUponAgingItem("foo", 0, 0);
+        AgingItem[] items = new AgingItem[]{spy};
+        GildedRose app = new GildedRose(items);
+
+        int updateCalls = 3;
+        for (int i = 0; i < updateCalls; i++) {
+            app.updateQuality();
+        }
+
+        assertEquals(spy.totalAgeIncrease, updateCalls);
     }
 
 }
