8d2a77a500255810982fe133a386cb76e062a311
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 08:51:55 +0100

    Make item field in AgingItem final

--- Java/src/main/java/com/gildedrose/agingitem/AgingItem.java
+++ Java/src/main/java/com/gildedrose/agingitem/AgingItem.java
@@ -3,7 +3,7 @@ package com.gildedrose.agingitem;
 import com.gildedrose.Item;
 
 public class AgingItem {
-    private Item item;
+    private final Item item;
 
     public AgingItem(String name, int sellIn, int quality) {
         item = new Item(name, sellIn, quality);
