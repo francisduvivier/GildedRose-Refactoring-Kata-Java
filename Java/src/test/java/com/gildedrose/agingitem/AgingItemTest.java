a6922e134aaaae257534556f139ac5d86d113124
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 09:02:43 +0100

    Add test for checking quality decrease as an item ages

--- Java/src/test/java/com/gildedrose/agingitem/AgingItemTest.java
+++ Java/src/test/java/com/gildedrose/agingitem/AgingItemTest.java
@@ -12,4 +12,22 @@ public class AgingItemTest {
         assertEquals(20, item.getQuality());
     }
 
+    @Test
+    public void decreasesQualityByOneWhenAgesBeforeExpiry() {
+        int startQuality = 20;
+        AgingItem item = new AgingItem("general", 10, startQuality);
+
+        item.increaseAge(1);
+        assertEquals(startQuality - 1, item.getQuality());
+    }
+
+    @Test
+    public void decreasesQualityByOneWhenAgesRightBeforeExpiry() {
+        int startQuality = 20;
+        int startSellIn = 10;
+        AgingItem item = new AgingItem("general", startSellIn, startQuality);
+
+        item.increaseAge(startSellIn);
+        assertEquals(startQuality - startSellIn, item.getQuality());
+    }
 }
