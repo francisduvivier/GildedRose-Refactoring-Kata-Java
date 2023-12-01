399e7157b86ad7d7ffad36ae6c4df8ae54b13a24
Francis Duvivier <francisduvivier@gmail.com>
2023-12-01 15:43:16 +0100

    Refactor to use LegendaryItem instead of CustomAgingItem

--- Java/src/main/java/com/gildedrose/agingitem/CustomAgingItem.java
+++ Java/src/main/java/com/gildedrose/agingitem/CustomAgingItem.java
@@ -9,9 +9,7 @@ public class CustomAgingItem extends AgingItem {
     public void increaseAge() {
         if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (this.quality > 0) {
-                if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    this.quality = this.quality - 1;
-                }
+                this.quality = this.quality - 1;
             }
         } else {
             if (this.quality < 50) {
@@ -30,21 +28,16 @@ public class CustomAgingItem extends AgingItem {
             }
         }
 
-        if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
-            this.sellIn = this.sellIn - 1;
-        }
+        this.sellIn = this.sellIn - 1;
 
         if (this.sellIn < 0) {
             if (!this.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
                 if (this.quality > 0) {
-                    if (!this.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        this.quality = this.quality - 1;
-                    }
+                    this.quality = this.quality - 1;
                 }
             } else {
                 this.quality = 0;
             }
         }
-
     }
 }
--- Java/src/test/java/com/gildedrose/TexttestFixture.java
+++ Java/src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,9 +1,6 @@
 package com.gildedrose;
 
-import com.gildedrose.agingitem.AgingItem;
-import com.gildedrose.agingitem.CustomAgingItem;
-import com.gildedrose.agingitem.StandardAgingItem;
-import com.gildedrose.agingitem.WellAgingItem;
+import com.gildedrose.agingitem.*;
 
 public class TexttestFixture {
     public static void main(String[] args) {
@@ -13,8 +10,8 @@ public class TexttestFixture {
             new StandardAgingItem("+5 Dexterity Vest", 10, 20), //
             new WellAgingItem("Aged Brie", 2, 0), //
             new CustomAgingItem("Elixir of the Mongoose", 5, 7), //
-            new CustomAgingItem("Sulfuras, Hand of Ragnaros", 0, 80), //
-            new CustomAgingItem("Sulfuras, Hand of Ragnaros", -1, 80),
+            new LegendaryItem("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new LegendaryItem("Sulfuras, Hand of Ragnaros", -1, 80),
             new CustomAgingItem("Backstage passes to a TAFKAL80ETC concert", 15, 20),
             new CustomAgingItem("Backstage passes to a TAFKAL80ETC concert", 10, 49),
             new CustomAgingItem("Backstage passes to a TAFKAL80ETC concert", 5, 49),
