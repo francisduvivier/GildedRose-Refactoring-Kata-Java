4be1e1953b5aa80b7269824616d490245b19f624
Francis Duvivier <francisduvivier@gmail.com>
2023-12-02 04:28:01 +0100

    Refactor Expiring Item classes to extend expiring Item

--- Java/src/main/java/com/gildedrose/agingitem/TicketItem.java
+++ Java/src/main/java/com/gildedrose/agingitem/TicketItem.java
@@ -1,13 +1,13 @@
 package com.gildedrose.agingitem;
 
-public class TicketItem extends AgingItem {
+public class TicketItem extends ExpiringItem {
     public TicketItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     @Override
     public void increaseAge() {
-        this.sellIn--;
+        super.increaseAge();
         if (this.sellIn < 0) {
             this.quality = 0;
             return;
--- Java/src/main/java/com/gildedrose/agingitem/WellAgingItem.java
+++ Java/src/main/java/com/gildedrose/agingitem/WellAgingItem.java
@@ -1,13 +1,13 @@
 package com.gildedrose.agingitem;
 
-public class WellAgingItem extends AgingItem {
+public class WellAgingItem extends ExpiringItem {
     public WellAgingItem(String name, int sellIn, int quality) {
         super(name, sellIn, quality);
     }
 
     @Override
     public void increaseAge() {
-        this.sellIn--;
+        super.increaseAge();
         if (this.quality >= MAX_ITEM_QUALITY) {
             return;
         }
